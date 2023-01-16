5c19e398ab651f93ceaf9f653a17e65fc049ba75
NKIyuki <u.y0701@icloud.com>
2023-01-16 12:59:27 +0000

    new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @book = @user/post_images
+    @post_images = @user.post_images
   end
 
   def edit
