1eedd172b79e10c14bc08074443d8671e80ef04b
NKIyuki <u.y0701@icloud.com>
2023-01-17 12:58:18 +0000

    show

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  resources :book,only:[:new,:index,:show]
-  get 'books/edit'
   root to: 'homes#top'
   devise_for :users
-  get 'books/:id' => 'homes#about',as:'books'
-  get 'homes/about'
+  get 'homes/about',as: 'about'
+  resources :users,only:[:show,:edit]
+  resources :books,only:[:new,:index,:show,:edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
