804ce6121cb04757b4d1e4d1c2a1e1488e86afcc
NKIyuki <u.y0701@icloud.com>
2023-01-18 13:31:17 +0000

    index

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+ has_one_attached :profile_image
+
   def get_profile_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
