32997f41e29df49fb8d478f61ae2e434d69a6b84
NKIyuki <u.y0701@icloud.com>
2023-01-20 12:43:40 +0000

    img

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,21 @@ class UsersController < ApplicationController
     @user = User.all
   end
 
+  def new
+    @post_images = Post_images.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'homes/about',as: 'about'
   resources :users,only:[:show,:edit,:index]
   resources :books,only:[:new,:index,:show,:edit]
+  resources :post_images,only:[:new,:show,:index,:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
