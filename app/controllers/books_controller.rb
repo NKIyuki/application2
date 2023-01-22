1bff43f21de1a5f4c320ed30078c10b2a17a33ee
NKIyuki <u.y0701@icloud.com>
2023-01-22 12:59:32 +0000

    sohw

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,10 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new
-    @book.user_id = current_user.id
+    @book = Book.new(book_params)
+    @book.user_id = current_user[:id]
     @book.save
-    redirect_to book_path(current_user)
+    redirect_to book_path(@book.id)
   end
 
   def index
@@ -15,10 +15,16 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+    @books = Book.find(params[:id])
+    @book = Book.new
     @user = current_user
   end
 
   def edit
   end
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
