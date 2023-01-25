f122d9f61fc56f888394bdb5c74bc4d0f208c860
NKIyuki <u.y0701@icloud.com>
2023-01-25 12:36:48 +0000

    index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
 
   def index
     @user = current_user
+    @books = Book.all
   end
 
   def show
@@ -30,15 +31,17 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
 
-  def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to book_path(book.id)
-  end
+
 
 end
