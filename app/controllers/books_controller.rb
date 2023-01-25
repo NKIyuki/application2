6efda6608852211e83e3d72b3f2b6e9a6e305d18
NKIyuki <u.y0701@icloud.com>
2023-01-25 13:12:14 +0000

    index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user[:id]
     @book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book.id)
   end
 
@@ -23,16 +24,19 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @book = Book.all
+    @user = current_user
   end
 
   def update
     book = Book.faind(params[:id])
     book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(book.id)
   end
 
   def destroy
-    @book = Book.find(params[:id])
+    @book = Book.find
     @book.destroy
     redirect_to books_path
   end
